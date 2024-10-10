287c315f64c210db755a70436f76f74ca3873e87
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-10-10 21:57:55 +0200

    updates main urls

--- backend/cooklist/urls.py
+++ backend/cooklist/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/', include('profiles.urls')),
+    path('profiles/', include('profiles.urls')),
 ]
