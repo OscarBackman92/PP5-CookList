0ba24e4dc34128589e68c0e59b03f304babea54d
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-10-10 22:36:47 +0200

    update to cooklist folder

--- backend/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Recipe
-from .serializers import RecipeSerializer
-from rest_framework import viewsets
-
-class RecipeViewSet(viewsets.ModelViewSet):
-    queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
--- backend/profiles/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Profile
-
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['user', 'bio', 'profile_picture', 'dietary_preferences', 'following']
-        read_only_fields = ['user']
