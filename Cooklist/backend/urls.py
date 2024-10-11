7c5505f331f13207a4207b9b5ac5a434f45a12c6
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-10-12 00:20:09 +0200

    update setting, views,urls,permissions,serilizers to Cooklist

--- Cooklist/backend/urls.py
+++ Cooklist/backend/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import root_route, logout_route
 
 urlpatterns = [
+    path('', root_route),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('profiles/', include('profiles.urls')),
+    path('api/auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+    ),
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('', include('api.urls')),
+    path('', include('profiles.urls')),
 ]
+
