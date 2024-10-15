0f696f24a56c61262aa981b83bda0507451fd3b6
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-10-15 12:34:13 +0200

    update recipe model

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Recipe
 
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('title', 'owner', 'created_at', 'updated_at')
+    list_display = ('title', 'author', 'created_at', 'updated_at')
     search_fields = ('title', 'owner__username')
     list_filter = ('created_at',)
 
--- api/models.py
+++ api/models.py
@@ -1,35 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
 
 class Recipe(models.Model):
-    image_filter_choices = [
-        ('_1977', '1977'),
-        ('brannan', 'Brannan'),
-        ('earlybird', 'Earlybird'),
-        ('hudson', 'Hudson'),
-        ('inkwell', 'Inkwell'),
-        ('lofi', 'Lo-Fi'),
-        ('kelvin', 'Kelvin'),
-        ('normal', 'Normal'),
-        ('nashville', 'Nashville'),
-        ('rise', 'Rise'),
-        ('toaster', 'Toaster'),
-        ('valencia', 'Valencia'),
-        ('walden', 'Walden'),
-        ('xpro2', 'X-pro II')
-    ]
-
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
-    image = models.ImageField(
-        upload_to='../images/', default='../images/default_post_vlvmq3', blank=True
-    )
-    image_filter = models.CharField(max_length=255, choices=image_filter_choices, null=True, blank=True)
+    image = models.ImageField(upload_to='images/')
     ingredients = models.TextField()
     instructions = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    preparation_time = models.PositiveIntegerField(help_text="Preparation time in minutes", default=0)
+    servings = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
 
     def __str__(self):
         return self.title
